3daec07a413a140e7edce8dd8bcdaf5a24cecdcc
luguizhou <422386546@qq.com>
2022-01-19 15:24:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const fs = require('fs')
 const path = require('path')
 
 const bytes = require('bytes')
+
+//获取当前进程pid根据pid 反馈memory和cup使用情况
 const pusage = require('pidusage')
+
+//内存快照 制定输出文件即可 使c++写的包
 const heapdump = require('heapdump')
+
+//已不可用 todo c++写的
 const profiler = require('v8-profiler')
+
+//已不可用 todo c++写的包
 const memwatch = require('memwatch-next')
+
+//控制速率的 在制定时间内，触发次数
 const RateLimiter = require('limiter').RateLimiter
 
 const processing = {
@@ -13,6 +23,7 @@ const processing = {
   memory: false
 }
 
+//统计出发快照次数
 const counter = {
   cpu: 0,
   memory: 0
@@ -43,6 +54,7 @@ function dumpCpu (cpuProfileDir, cpuDuration) {
   }, cpuDuration)
 }
 
+//根据传过来的文件地址生产快照
 function dumpMemory (memProfileDir, isLeak = false) {
   processing.memory = true
   const filepath = genProfilePath(memProfileDir, isLeak ? 'leak-memory' : 'memory', 'heapsnapshot')
@@ -110,10 +122,14 @@ module.exports = function cpuMemoryMonitor (options = {}) {
   }
 
   if (options.memory) {
+    
+    //根据memInterval 定时执行判断是否要生成快照
     const memTimer = setInterval(() => {
       if (processing.memory) {
         return
       }
+      
+      //根据process.pid 获取当前进程的cpu和memory情况
       pusage.stat(process.pid, (err, stat) => {
         if (err) {
           console.error(`memory stat error: ${err.message}`)
@@ -121,15 +137,21 @@ module.exports = function cpuMemoryMonitor (options = {}) {
           clearInterval(memTimer)
           return
         }
+        
+        //达到阈值触发
         if (stat.memory > memThreshold) {
           counter.memory += 1
           if (counter.memory >= memCounter) {
+            
+            //根据速率控制器 出发dump动作
             cpuLimiter.removeTokens(1, (limiterErr, remaining) => {
               if (limiterErr) {
                 console.error(`limiterErr: ${limiterErr.message}`)
                 console.error(limiterErr.stack)
                 return
               }
+              
+              //当达到频次阈值后，触发。为了控制粗发频次，触发阈值memLimiter次后 ，dump
               if (remaining > -1) {
                 dumpMemory(memProfileDir)
                 counter.memory = 0
@@ -142,6 +164,7 @@ module.exports = function cpuMemoryMonitor (options = {}) {
       })
     }, memInterval)
 
+    //包兼容问题已不可用，c++写的包
     memwatch.on('leak', (info) => {
       console.warn('memory leak: %j', info)
       dumpMemory(memProfileDir, true)
